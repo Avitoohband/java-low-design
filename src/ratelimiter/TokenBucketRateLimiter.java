49d2c316df40fda3967d219fb6dbc18ccc5112a7
Avi Tuchband <avitoohband@gmail.com>
2023-12-03 19:50:05 +0200

    implemented rate limiter

---
+++ src/ratelimiter/TokenBucketRateLimiter.java
@@ -0,0 +1,47 @@
+package ratelimiter;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class TokenBucketRateLimiter {
+    private final Object lock = new Object();
+    private final int tokensPerRefill;
+    private final long timeBetweenRefills;
+    private final int maxBucketSize;
+    private final AtomicInteger tokensAvailable;
+
+    long lastRefillTime;
+
+
+    public TokenBucketRateLimiter(int maxBucketSize, int tokensPerRefill, long timeBetweenRefills) {
+        this.timeBetweenRefills = timeBetweenRefills;
+        this.maxBucketSize = maxBucketSize;
+        this.tokensPerRefill = tokensPerRefill;
+        this.tokensAvailable = new AtomicInteger(0);
+    }
+
+    public boolean tryConsumeToken() {
+        synchronized (lock) {
+            refill();
+            if (tokensAvailable.get() > 0) {
+                tokensAvailable.decrementAndGet();
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    private void refill() {
+        synchronized (lock) {
+            long currentTimeMillis = System.currentTimeMillis();
+            if ((currentTimeMillis - lastRefillTime) > timeBetweenRefills) {
+                lastRefillTime = currentTimeMillis;
+                tokensAvailable.updateAndGet(
+                        current ->
+                                Math.min(
+                                        maxBucketSize, current + tokensPerRefill
+                                ));
+            }
+        }
+    }
+}
---
+++ src/test/ratelimiter/Main.java
@@ -0,0 +1,48 @@
+package test.ratelimiter;
+
+import ratelimiter.Constants;
+import ratelimiter.TokenBucketRateLimiter;
+
+public class Main {
+    public static void main(String[] args) {
+        TokenBucketRateLimiter tokenBucketRateLimiter = new TokenBucketRateLimiter(
+                Constants.maxBucketSize, Constants.tokensPerRefill, Constants.timeBetweenRefills
+                );
+
+        int numberOfSuccessfulConsumes = 0;
+        int numberOfUnsuccessfulConsumes = 0;
+
+        long startTimeMillis = System.currentTimeMillis();
+
+        while((System.currentTimeMillis() - startTimeMillis) <= 10_000L){
+            boolean isSucceed = tokenBucketRateLimiter.tryConsumeToken();
+
+            if(isSucceed){
+                System.out.println("Consume Succeed");
+                numberOfSuccessfulConsumes++;
+            }else {
+                numberOfUnsuccessfulConsumes++;
+                if(numberOfUnsuccessfulConsumes % 1_000_000_00 == 0){
+                    System.out.println(numberOfUnsuccessfulConsumes + ": requests have been blocked.");
+
+                }
+            }
+        }
+
+        long endTimeMillis = System.currentTimeMillis();
+
+        System.out.println();
+        System.out.println("The time the process took: " + (endTimeMillis - startTimeMillis));
+        System.out.println("Number of successful consumes: " + numberOfSuccessfulConsumes);
+        System.out.println("Number of expected consumes: " + Constants.tokensPerRefill * 10);
+        System.out.println("A total of: " + numberOfUnsuccessfulConsumes + " requests have been blocked.");
+
+
+
+
+    }
+
+
+
+
+}
